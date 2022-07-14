a1725324cbbe5fe53a5c8228d817a32c03bc9903
narayananias <71780885+narayananias@users.noreply.github.com>
2022-07-14 19:11:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import './TicTacToe.css';
 import Game from './Game.js';
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h2>Welcome to TicTacToe</h2>
         </div>
         <p className="App-intro">
